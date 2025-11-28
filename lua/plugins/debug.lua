c41b33106122ef62bb30b1cfa1574eec9693716c
thinkphat <nguyenthinhphat3009@gmail.com>
2025-11-28 20:46:09 +0700

    add virtual-text

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -2,6 +2,7 @@ return {
   "mfussenegger/nvim-dap",
   dependencies = {
     "rcarriga/nvim-dap-ui",
+    "theHamsta/nvim-dap-virtual-text",
     "nvim-neotest/nvim-nio",
     "williamboman/mason.nvim",
     "jay-babu/mason-nvim-dap.nvim",
@@ -19,11 +20,13 @@ return {
       },
     })
     require("dap-go").setup()
+    require("nvim-dap-virtual-text").setup()
 
     vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
     vim.keymap.set("n", "<F1>", dap.step_into, { desc = "Debug: Step Into" })
     vim.keymap.set("n", "<F2>", dap.step_over, { desc = "Debug: Step Over" })
     vim.keymap.set("n", "<F3>", dap.step_out, { desc = "Debug: Step Out" })
+    vim.keymap.set("n", "<F4>", dap.run_last, { desc = "Debug: Step Back" })
     vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
     vim.keymap.set("n", "<leader>B", function()
       dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
